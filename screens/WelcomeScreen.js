ff60ab6828d4864ed15356b6c2c4315b3d3d1b11
Jeff Wilkey <jeffrotull92@gmail.com>
2018-05-24 15:04:24 -0700

    hide welcome screen if token already exists

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,5 +1,7 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, AsyncStorage } from 'react-native';
+import { AppLoading } from 'expo';
 import Slides from '../components/Slides';
 
 const SLIDE_DATA = [
@@ -9,11 +11,27 @@ const SLIDE_DATA = [
 ];
 
 class WelcomeScreen extends Component {
-  onSlidesComplete = () => {
-    this.props.navigation.navigate('auth');
+  state = { token: null };
+
+  async componentWillMount() {
+    let token = await AsyncStorage.getItem('fb_token');
+    if (token) {
+      this.props.navigation.navigate('map');
+      this.setState({ token });
+    } else {
+      this.setState({ token: false });
+    }
   }
 
+  onSlidesComplete = () => {
+    this.props.navigation.navigate('auth');
+  };
+
   render() {
+    if (_.isNull(this.state.token)) {
+      return <AppLoading />;
+    }
+
     return (
       <View style={{ flex: 1 }}>
         <Slides data={SLIDE_DATA} onComplete={this.onSlidesComplete} />
