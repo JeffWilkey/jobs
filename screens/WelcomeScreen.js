4ee5e37dd1a7cc8754168a27e1235bf557c2aacf
Jeff Wilkey <jeffrotull92@gmail.com>
2018-05-24 11:09:22 -0700

    remove uncessary import of Text tag

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import Slides from '../components/Slides';
 
 const SLIDE_DATA = [
